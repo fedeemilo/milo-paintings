82f45d1a55dfda87fc8d75b9f39ca19926ca1c95
Federico Milone <federico.milone@claro.com.ar>
2022-11-18 15:12:15 -0300

    Add more acuarela paintings

--- src/assets/dummy.js
+++ src/assets/dummy.js
@@ -1,78 +1,198 @@
-import { MEDIA_DATA } from '../constants/imgs';
+import { MEDIA_DATA } from "../constants/imgs";
 
 export const PAINT_CARDS = [
     {
         key: 1,
         src: MEDIA_DATA.BODEGON_ANTIGUO,
-        altText: 'Bodegon Antiguo',
-        caption: 'Óleo sobre lienzo',
+        altText: "Bodegon Antiguo",
+        caption: "Óleo sobre lienzo",
         price: 290
     },
     {
         key: 2,
         src: MEDIA_DATA.BODEGON_LA_BODEGA,
-        altText: 'Bodegon - La Bodega',
-        caption: 'Óleo sobre tabla de lenga',
+        altText: "Bodegon - La Bodega",
+        caption: "Óleo sobre tabla de lenga",
         price: 300
     },
     {
         key: 3,
         src: MEDIA_DATA.BODEGON_THREE,
-        altText: 'Bodegon 3',
-        caption: 'Óleo sobre lienzo',
+        altText: "Bodegon 3",
+        caption: "Óleo sobre lienzo",
         price: 250
     },
     {
         key: 4,
         src: MEDIA_DATA.BODEGON_FOUR,
-        altText: 'Bodegon 4',
-        caption: 'Óleo sobre lienzo',
+        altText: "Bodegon 4",
+        caption: "Óleo sobre lienzo",
         price: 250
     },
     {
         key: 5,
         src: MEDIA_DATA.BODEGON_FIVE,
-        altText: 'Bodegon 5',
-        caption: 'Óleo sobre lienzo',
+        altText: "Bodegon 5",
+        caption: "Óleo sobre lienzo",
         price: 250
     },
     {
         key: 6,
         src: MEDIA_DATA.BODEGON_SIX,
-        altText: 'Bodegon 6',
-        caption: 'Óleo sobre lienzo',
+        altText: "Bodegon 6",
+        caption: "Óleo sobre lienzo",
         price: 300
     },
     {
         key: 7,
         src: MEDIA_DATA.CIERVOS_EN_OTONO,
-        altText: 'Ciervos en Otoño',
-        caption: 'Óleo sobre lienzo',
+        altText: "Ciervos en Otoño",
+        caption: "Óleo sobre lienzo",
         price: 300
     },
     {
         key: 8,
         src: MEDIA_DATA.PAISAJE_NEVADO,
-        altText: 'Paisaje Nevado',
-        caption: 'Óleo sobre lienzo',
+        altText: "Paisaje Nevado",
+        caption: "Óleo sobre lienzo",
         price: 220
+    },
+
+    {
+        key: 9,
+        src: MEDIA_DATA.ACUARELA_CONTRALUZ_1,
+        altText: "Contraluz 1",
+        caption: "Acuarela",
+        price: 90
+    },
+    {
+        key: 10,
+        src: MEDIA_DATA.ACUARELA_EL_CAMPITO,
+        altText: "EL Campito",
+        caption: "Acuarela",
+        price: 90
+    },
+    {
+        key: 11,
+        src: MEDIA_DATA.ACUARELA_EL_MUELLE,
+        altText: "El Muelle",
+        caption: "Acuarela",
+        price: 80
+    },
+    {
+        key: 12,
+        src: MEDIA_DATA.ACUARELA_FLORES_SILVESTRES,
+        altText: "Flores Silvestres",
+        caption: "Acuarela",
+        price: 70
+    },
+    {
+        key: 13,
+        src: MEDIA_DATA.ACUARELA_LA_SIEMBRA,
+        altText: "La Siembra",
+        caption: "Acuarela",
+        price: 100
+    },
+    {
+        key: 14,
+        src: MEDIA_DATA.ACUARELA_LOLOG,
+        altText: "Lolog",
+        caption: "Acuarela",
+        price: 100
+    },
+    {
+        key: 15,
+        src: MEDIA_DATA.ACUARELA_PAISAJE_2,
+        altText: "Paisaje 2",
+        caption: "Acuarela",
+        price: 110
+    },
+    {
+        key: 16,
+        src: MEDIA_DATA.ACUARELA_PAISAJE_3,
+        altText: "Paisaje 3",
+        caption: "Acuarela",
+        price: 90
+    },
+    {
+        key: 17,
+        src: MEDIA_DATA.ACUARELA_PAISAJE_4,
+        altText: "Paisaje 4",
+        caption: "Acuarela",
+        price: 110
+    },
+    {
+        key: 18,
+        src: MEDIA_DATA.ACUARELA_PAISAJE_5,
+        altText: "Paisaje 5",
+        caption: "Acuarela",
+        price: 110
+    },
+    {
+        key: 19,
+        src: MEDIA_DATA.ACUARELA_PAISAJE_6,
+        altText: "Paisaje 6",
+        caption: "Acuarela",
+        price: 80
+    },
+    {
+        key: 20,
+        src: MEDIA_DATA.ACUARELA_PAISAJE_NEVADO_1,
+        altText: "Paisaje Nevado 1",
+        caption: "Acuarela",
+        price: 110
+    },
+    {
+        key: 21,
+        src: MEDIA_DATA.ACUARELA_PAISAJE_NEVADO_2,
+        altText: "Paisaje Nevado 2",
+        caption: "Acuarela",
+        price: 110
+    },
+    {
+        key: 22,
+        src: MEDIA_DATA.ACUARELA_PLAYA_1,
+        altText: "Playa 1",
+        caption: "Acuarela",
+        price: 100
+    },
+    {
+        key: 23,
+        src: MEDIA_DATA.ACUARELA_RANCHO_1,
+        altText: "Rancho 1",
+        caption: "Acuarela",
+        price: 90
+    },
+    {
+        key: 24,
+        src: MEDIA_DATA.ACUARELA_SORPRESA,
+        altText: "Sorpresa",
+        caption: "Acuarela",
+        price: 100
+    },
+    {
+        key: 25,
+        src: MEDIA_DATA.ACUARELA_VIEJA_CANILLA,
+        altText: "Vieja Canilla",
+        caption: "Acuarela",
+        price: 100
     }
 ];
 
 export const BACKGROUND_PATTERNS = [
     {
         key: 1,
-        name: 'Triángulos',
+        name: "Triángulos",
         pattern: MEDIA_DATA.TRIANGLES_PATTERN
     },
     {
         key: 2,
-        name: 'Mosaicos',
+        name: "Mosaicos",
         pattern: MEDIA_DATA.MOSAIC_PATTERN
     },
     {
         key: 3,
-        name: 'Tubos',
+        name: "Tubos",
         pattern: MEDIA_DATA.PIPES_PATTERN
     }
 ];
