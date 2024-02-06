e31f7384e5107ee4b09868c8d624e035bd463800
Federico Milone <federico.milone@claro.com.ar>
2024-02-06 18:37:47 -0300

    Add more timeout maxDuration on vercel.json

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -3,6 +3,6 @@ import { isLocalhost } from '../utils'
 const URL_API_LOCAL = 'http://localhost:8080/api/paintings'
 const URL_API_PROD = 'https://milo-paintings-backend.vercel.app/api/paintings'
 
-const URL_API = isLocalhost ? URL_API_LOCAL : URL_API_PROD
+const URL_API = !isLocalhost ? URL_API_LOCAL : URL_API_PROD
 
 export { URL_API }
